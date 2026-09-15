f51a3c825ecefee2d867b9608b372ed15447508f
scordill <scordill@fcgov.com>
2026-09-15 13:14:40 -0600

    Add new fields to form

--- resources/js/pages/auth/Register.vue
+++ resources/js/pages/auth/Register.vue
@@ -33,16 +33,44 @@ defineOptions({
     >
         <div class="grid gap-6">
             <div class="grid gap-2">
-                <Label for="name">Name</Label>
+                <Label for="first_name">First Name</Label>
                 <Input
-                    id="name"
+                    id="first_name"
                     type="text"
                     required
                     autofocus
                     :tabindex="1"
-                    autocomplete="name"
-                    name="name"
-                    placeholder="Full name"
+                    autocomplete="first_name"
+                    name="first_name"
+                    placeholder="First name"
+                />
+                <InputError :message="errors.name" />
+            </div>
+            <div class="grid gap-2">
+                <Label for="last_name">Last Name</Label>
+                <Input
+                    id="last_name"
+                    type="text"
+                    required
+                    autofocus
+                    :tabindex="1"
+                    autocomplete="last_name"
+                    name="last_name"
+                    placeholder="Last name"
+                />
+                <InputError :message="errors.name" />
+            </div>
+            <div class="grid gap-2">
+                <Label for="display_name">Display Name</Label>
+                <Input
+                    id="display_name"
+                    type="text"
+                    required
+                    autofocus
+                    :tabindex="1"
+                    autocomplete="display_name"
+                    name="display_name"
+                    placeholder="Display name"
                 />
                 <InputError :message="errors.name" />
             </div>
