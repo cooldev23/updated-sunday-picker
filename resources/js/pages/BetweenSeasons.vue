8b431c1b9a5c6134d43c049bfda73df09ac1d107
scordill <scordill@fcgov.com>
2026-09-17 08:22:19 -0600

    Create new view

---
+++ resources/js/pages/BetweenSeasons.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { dashboard } from '@/routes';
+
+defineOptions({
+    layout: {
+        breadcrumbs: [
+            {
+                title: 'Dashboard',
+                href: dashboard(),
+            },
+            {
+                title: 'Between Seasons',
+                href: null,
+            },
+        ],
+    },
+});
+</script>
+
+<template>
+  <!-- TODO: replace year with year from current week table -->
+  <h1 class="text-2xl">The 2026 NFL season has not started yet.</h1>
+   <p>Season starts on: </p> <!-- TODO: how to get this -->
+</template>
