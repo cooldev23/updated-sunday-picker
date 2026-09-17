5a581aef90901236e7b91f95117aa8ed9f1a9501
scordill <scordill@fcgov.com>
2026-09-17 08:22:32 -0600

    Admin stuff

---
+++ resources/js/pages/admin/Dashboard.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { Link, Head } from '@inertiajs/vue3';
+import { getNflData, adminDashboard } from '@/routes/admin';
+
+defineOptions({
+    layout: {
+        breadcrumbs: [
+            {
+                title: 'Admin Dashboard',
+                href: adminDashboard(),
+            }
+        ],
+    },
+});
+</script>
+
+<template>
+  This is the admin page
+  <Link :href="getNflData()">
+    getNflData
+  </Link>
+</template>
