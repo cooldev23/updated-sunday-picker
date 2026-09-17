f2bd5d7fabd6087017d704051c8931e455517ec0
scordill <scordill@fcgov.com>
2026-09-17 08:18:59 -0600

    Move this to a different file

--- resources/js/pages/admin/nflData/GetNFLData.vue
+++ resources/js/pages/admin/nflData/GetNFLData.vue
@@ -1,19 +1,32 @@
 <script setup>
-import { ref } from 'vue';
-import { Head, Link } from '@inertiajs/vue3';
-import axios from 'axios';
-import Card from '@/components/ui/card/Card.vue';
-import { UserGroupIcon, CalendarDaysIcon } from '@heroicons/vue/24/solid';
+import { Head, Link, usePage } from '@inertiajs/vue3';
+import { CalendarDays, UsersRoundIcon } from '@lucide/vue';
+import { 
+  Card,
+  CardTitle,
+  CardHeader,
+  CardContent,
+} from '@/components/ui/card';
+import { store } from '@/routes/admin/nflSchedule';
+import { storeTeams } from '@/routes/admin/nflTeams';
+import { getNflData, adminDashboard } from '@/routes/admin';
 
-const message = ref('');
-const currentYear = new Date().getFullYear();
+defineOptions({
+    layout: {
+        breadcrumbs: [
+            {
+                title: 'Admin Dashboard',
+                href: adminDashboard(),
+            },
+            {
+                title: 'Get NFL Data',
+                href: getNflData(),
+            },
+        ],
+    },
+});
 
-function submit() {
-  axios.post('create-schedule')
-    .then(response => {
-      message.value = response.data.success
-    })
-}
+const page = usePage();
 </script>
 
 <template>
@@ -21,27 +34,35 @@ function submit() {
   <Head title="Get Data"></Head>
 
   <div class="p-3">
-    <h1 class="mb-2 pb-1 text-xl border-b">Get NFL Data</h1>
-    <div class="grid grid-cols-4 gap-x-4">
-      <Link :href="route('admin.nflSchedule.store')">
-        <Card class="p-3 flex flex-col items-center hover:shadow-sm">
+    <!-- <h1 class="mb-2 pb-1 text-xl border-b">Get NFL Data</h1> -->
+    <div v-if="page.flash.toast" class="toast">
+      {{ page.flash.toast.message }}
+    </div>
+    <div class="grid grid-cols-1 gap-y-4 gap-x-4 md:grid-cols-4 md:gap-y-0">
+      <Link :href="store()">
+        <Card class="text-center hover:shadow-sm">
           <CardHeader>
             <CardTitle>Get Schedule</CardTitle>
           </CardHeader>
-          <CalendarDaysIcon class="w-12 h-12"/>
+          <div class="flex justify-center">
+            <CalendarDays class="w-12 h-12"/>
+          </div>
           <CardContent>
             <p className="text-sm text-muted-foreground">
-              Get {{ currentYear }} NFL games
+              <!-- TODO: replace this with year from current_week table -->
+              Get 2026 NFL games
             </p>
           </CardContent>
         </Card>
       </Link>
-      <Link :href="route('admin.nflTeams.store')">
-        <Card class="p-3 flex flex-col items-center hover:shadow-sm">
+      <Link :href="storeTeams()">
+        <Card class="text-center hover:shadow-sm">
           <CardHeader>
             <CardTitle>Get Teams</CardTitle>
           </CardHeader>
-          <UserGroupIcon class="w-12 h-12"/>
+          <div class="flex justify-center">
+            <UsersRoundIcon class="w-12 h-12"/>
+          </div>
           <CardContent>
             <p className="text-sm text-muted-foreground">
               Get NFL teams
@@ -49,8 +70,6 @@ function submit() {
           </CardContent>
         </Card>
       </Link>
-      <!-- TODO: Set Weeks in current weeks table -->
-      <!-- Anything else? -->
     </div>
   </div>
 </template>
