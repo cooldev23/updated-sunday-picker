021f87c6febd737f8480ae4d4e89918d8dc06580
scordill <scordill@fcgov.com>
2026-09-21 18:32:24 -0600

    Trying to get pinia going

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,28 +1,41 @@
 import { createInertiaApp } from '@inertiajs/vue3';
-import { createApp, h } from 'vue'
+import { createApp, h } from 'vue';
 import { initializeTheme } from '@/composables/useAppearance';
 import AppLayout from '@/layouts/AppLayout.vue';
 import AuthLayout from '@/layouts/AuthLayout.vue';
 import SettingsLayout from '@/layouts/settings/Layout.vue';
 import { initializeFlashToast } from '@/lib/flashToast';
-import { ZiggyVue } from 'ziggy-js';
-import { route } from 'ziggy-js';
+import { createPinia } from 'pinia';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 void createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
-    layout: (name) => {
-        switch (true) {
-            case name === 'Welcome':
-                return null;
-            case name.startsWith('auth/'):
-                return AuthLayout;
-            case name.startsWith('settings/'):
-                return [AppLayout, SettingsLayout];
-            default:
-                return AppLayout;
+    resolve: (name) => {
+        const pages = import.meta.glob('./Pages/**/*.vue', { eager: true });
+        const page = pages[`./Pages/${name}.vue`].default;
+        
+        // Apply your dynamic layouts
+        if (name === 'Welcome') {
+            page.layout = null;
+        } else if (name.startsWith('auth/')) {
+            page.layout = AuthLayout;
+        } else if (name.startsWith('settings/')) {
+            page.layout = [AppLayout, SettingsLayout];
+        } else {
+            page.layout = page.layout || AppLayout;
         }
+        
+        return page;
+    },
+    setup({ el, App, props, plugin }) {
+        console.log(el);
+        const pinia = createPinia();
+
+        const app = createApp({ render: () => h(App, props) })
+            app.use(plugin);
+            app.use(pinia);
+            app.mount(el);
     },
     progress: {
         color: '#4B5563',
