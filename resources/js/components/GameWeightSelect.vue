79d56169287167c91530a4d9f5990bde43dd75db
scordill <scordill@fcgov.com>
2026-09-23 16:07:18 -0600

    Minor style updates

--- resources/js/components/GameWeightSelect.vue
+++ resources/js/components/GameWeightSelect.vue
@@ -25,7 +25,7 @@ function updateWeight() {
     :class="{ 'text-red-600': hasWeightError }, { 'text-green-400': userSelectedWeight !== 0 }"
     :for="gameId">Weight</label>
   <select :id="gameId"
-    class="w-20 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+    class="p-1.5 w-20 rounded-md border-0 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
     v-model="userSelectedWeight" @change="updateWeight()"
     :class="{ 'ring-red-600 text-red-600': hasWeightError }, { 'ring-green-400': userSelectedWeight !== 0 }">
     <option v-for="(num, index) in selectedTeamsStore.weekGamesCount" :key="index" :value="num">{{ num }}</option>
